341d4bdffe7adbe67d0ed039f7f197dfba36da19
ridertus <ridertus@hotmail.com>
2024-07-02 16:39:24 +0700

    Update ForgotPassword

--- smartbin/src/components/Forgot-password/ForgotPassword.jsx
+++ smartbin/src/components/Forgot-password/ForgotPassword.jsx
@@ -4,48 +4,47 @@ export default function Forgot() {
   return (
     <>
       <section className="main-container">
-        <header className="header">
-          <div className="smart-bin-icon">SMART BIN ICON</div>
-        </header>
-        <div className="content">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/fef8381d04643b55c1fca60fa2e9c0ad2cc8083567944c536841bf54f7bd3e87?apiKey=e36162eb1f314ee4a7604edd117f93c9&"
-            alt="Smart Bin Icon"
-            className="smart-bin-image"
-          />
+        <header className="header">SMART BIN ICON</header>
+        <div className="registration-container">
           <h1 className="title">รีเซ็ตรหัสผ่านของคุณ</h1>
           <p className="description">
             ป้อนหมายเลขโทรศัพท์ของคุณ ที่ลงทะเบียนไว้กับ Smart Bin ที่นี่
           </p>
-          <label htmlFor="phoneNumberInput" className="sr-only">
-            หมายเลขโทรศัพท์
-          </label>
-          <input
-            className="phone-input"
-            type="tel"
-            id="phoneNumberInput"
-            placeholder="หมายเลขโทรศัพท์"
-            aria-label="หมายเลขโทรศัพท์"
-          />
-          <button className="submit-button" type="submit">
-            ยืนยัน
-          </button>
+          <form className="form">
+            {/* <label className="label" htmlFor="phoneNumberInput">
+              หมายเลขโทรศัพท์
+            </label> */}
+            <input
+              className="input"
+              type="tel"
+              id="phoneNumberInput"
+              placeholder="หมายเลขโทรศัพท์"
+              aria-label="หมายเลขโทรศัพท์"
+            />
+            <button className="submit-button" type="submit">
+              ยืนยัน
+            </button>
+          </form>
         </div>
       </section>
       <style jsx>{`
-        .sr-only {
-          position: absolute;
-          width: 1px;
-          height: 1px;
-          margin: -1px;
-          padding: 0;
-          overflow: hidden;
-          clip: rect(0, 0, 0, 0);
-          border: 0;
+        :root {
+          --primary-color: #9E76B4;
+          --primary-color-container: #fff;
+          --secondary-color: #000;
+          --secondary-color-font: #fff;
+          --font-family: 'Mitr', sans-serif;
+          --font-family-secondary: 'Roboto', sans-serif;
+          --font-size-base: 18px;
+          --font-size-large: 18px;
+          --font-size-small: 16px;
+          --border-radius: 20px;
+          --box-shadow: 0px 0.05px 2px 1px rgba(0, 0, 0, 0.25);
+          --box-shadow-button: 0px 1px 1px rgba(0, 0, 0, 0.25), 0px -1px 1px rgba(0, 0, 0, 0.25);
         }
+
         .main-container {
-          background-color: #fff;
+          background-color: var(--primary-color-container);
           display: flex;
           width: auto;
           padding: 53px 17px;
@@ -55,81 +54,87 @@ export default function Forgot() {
           justify-content: center;
           align-items: center;
         }
+
         .header {
-          backdrop-filter: blur(50px);
-          background-color: rgba(255, 255, 255, 0.7);
+          border-radius: var(--border-radius);
+          box-shadow: var(--box-shadow-button);
+          background-color: var(--primary-color-container);
+          align-self: center;
+          margin-top: 19px;
+          width: 115px;
+          max-width: 100%;
+          color: var(--secondary-color);
+          text-align: center;
+          justify-content: center;
+          padding: 12px 17px;
+          font: 700 16px var(--font-family);
+        }
+
+        .registration-container {
+          border-radius: var(--border-radius);
+          box-shadow: var(--box-shadow-button);
+          background-color: var(--primary-color-container);
           display: flex;
+          margin-top: 20px;
+          max-width: 480px;
           width: 100%;
           flex-direction: column;
-          padding: 80px 20px 0;
-          text-align: center;
+          padding: 10px 17px;
         }
-        .smart-bin-icon {
-          margin-top: 30px;
-          font: 700 16px Mitr, sans-serif;
-        }
-        .content {
-          border-radius: 20px;
-          box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.25),
-            0px -1px 4px 0px rgba(0, 0, 0, 0.25);
-          background-color: #fff;
-          display: flex;
-          width: auto;
-          flex-direction: column;
-          align-items: flex-start;
-          font-size: 15px;
-          font-weight: 400;
-          margin: 0 auto;
-          padding: 53 17px;
-        }
-        .smart-bin-image {
-          aspect-ratio: 1;
-          object-fit: auto;
-          object-position: center;
-          width: 24px;
-        }
+
         .title {
-          margin: 20px 0 0 12px;
-          font: 700 20px Mitr, sans-serif;
+          color: var(--secondary-color);
+          font: 700 25px var(--font-family);
+          margin-bottom: 0px;
         }
+
         .description {
-          font-family: Mitr, sans-serif;
-          padding-right: 54px;
-          margin: 0 -3px 0 14px;
+          font-family: var(--font-family);
+          margin: 5px 0;
+          margin-bottom: 0px;
+          color: var(--secondary-color);
+          font-size: 16px;
         }
-        .form-wrapper {
+
+        .form {
           display: flex;
           flex-direction: column;
-          align-items: start;
-          margin: 15px 0 0 14px;
         }
-        .phone-input {
+
+        .label {
+          color: var(--secondary-color);
+          margin-top: 10px;
+          font: 600 15px var(--font-family);
+        }
+
+        .input {
           border-radius: 10px;
-          box-shadow: 0px 0.05px 2px 0px rgba(0, 0, 0, 0.25) inset;
-          background-color: #fff;
-          margin: 10px -3px 0 14px;
+          box-shadow: var(--box-shadow) inset;
+          border: none;
+          background-color: var(--primary-color-container);
+          margin-top: 6px;
           align-items: start;
-          color: #000;
+          color: var(--secondary-color);
           justify-content: center;
           padding: 11px 12px;
-          font: 400 15px Mitr, sans-serif;
+          font: 400 15px var(--font-family);
         }
+
         .submit-button {
-          border-radius: 20px;
-          box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25), 0px -1px 1px rgba(0, 0, 0, 0.25);
-          background-color: #fff;
+          border-radius: var(--border-radius);
+          box-shadow: var(--box-shadow-button);
+          background-color: var(--primary-color);
           align-self: center;
           margin-top: 15px;
           width: 199px;
           max-width: 100%;
           align-items: center;
-          border-color: #fff;
-          color: #000;
+          border: none;
+          color: var(--secondary-color-font);
           text-align: center;
           justify-content: center;
           padding: 12px 60px;
-          font: 400 15px Mitr, sans-serif;
-          margin: 20px 0 15px;
+          font: 400 15px var(--font-family);
         }
       `}</style>
     </>
